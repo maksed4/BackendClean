3a171e6f259633d5a05e42dd8b3788bccce52722
Youssef Al Shadidi <youssef.as@gmail.com>
2022-10-20 10:33:18 +0200

    Our init commit

---
+++ src/main/java/se/ecutbildning/cleanice/CleaniceApplication.java
@@ -0,0 +1,13 @@
+package se.ecutbildning.cleanice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaniceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleaniceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/ecutbildning/cleanice/CleaniceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ecutbildning.cleanice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaniceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
