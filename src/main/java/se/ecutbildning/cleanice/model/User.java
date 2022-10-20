41eee8ad17b9c696871add24a4f3dafe3ba8bf8a
Youssef Al Shadidi <youssef.as@gmail.com>
2022-10-20 20:58:36 +0200

    IN-5 Added folder structure

--- src/main/java/se/ecutbildning/cleanice/model/User.java
+++ src/main/java/se/ecutbildning/cleanice/model/User.java
@@ -5,6 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Id;
+
 @Getter @Setter @ToString
 public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
 }
